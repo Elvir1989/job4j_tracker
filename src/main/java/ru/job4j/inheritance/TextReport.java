0225f28ced2dfd690819ae846df529f178b738c8
Elvir1989 <elvirpovar@gmail.com>
2023-02-19 22:22:21 +0300

    4. Переопределение метода

--- src/main/java/ru/job4j/inheritance/TextReport.java
+++ src/main/java/ru/job4j/inheritance/TextReport.java
@@ -4,10 +4,4 @@ public class TextReport {
     public String generate(String name, String body) {
         return name + System.lineSeparator() + body;
     }
-
-    public static void main(String[] args) {
-        JSONReport js = new JSONReport();
-        String text = js.generate("name", "body");
-        System.out.println(text);
-    }
 }
