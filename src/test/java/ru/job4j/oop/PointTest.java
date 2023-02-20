c51cca996041d2c4e525562b7bb003d8839215d3
Elvir1989 <elvirpovar@gmail.com>
2023-02-20 09:54:48 +0300

    2.0. Рефакторинг - Расстояние между точками.

---
+++ src/test/java/ru/job4j/oop/PointTest.java
@@ -0,0 +1,43 @@
+package ru.job4j.oop;
+
+import org.junit.Assert;
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PointTest {
+    @Test
+    public void when00to20then2() {
+        double expected = 0;
+        int x = 0;
+        int y = 0;
+        double out = new Point(x, y).distance(new Point(x, y));
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when11to01then1() {
+        double expected = 0;
+        int x = 1;
+        int y = 1;
+        double out = new Point(x, y).distance(new Point(x, y));
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when20to00then2() {
+        double expected = 0;
+        int x = 2;
+        int y = 0;
+        double out = new Point(x, y).distance(new Point(x, y));
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when01to21then2() {
+        double expected = 0;
+        int x = 0;
+        int y = 1;
+        double out = new Point(x, y).distance(new Point(x, y));
+        Assert.assertEquals(expected, out, 0.01);
+    }
+}
