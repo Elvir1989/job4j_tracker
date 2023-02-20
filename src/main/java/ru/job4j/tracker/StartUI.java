2ccb1303074946f9864b1e6e8231bf061a3d0da0
Elvir1989 <elvirpovar@gmail.com>
2023-02-20 17:12:43 +0300

    8. Date. Отображение даты.

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,16 @@
+package ru.job4j.tracker;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class StartUI extends Item {
+
+    public static void main(String[] args) {
+
+        Item item = new Item();
+        LocalDateTime created = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        String createdDateTimeFormat = created.format(formatter);
+        System.out.println("Текущее дата и время " + createdDateTimeFormat);
+    }
+}
